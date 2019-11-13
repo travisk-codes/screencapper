42087d9c4fd54f89f079fe4075c3e6cb134c5d90
Travis Kohlbeck <me+git@travisk.info>
2019-11-13 08:51:55 -0500

    cleans up, some polish

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-const request = require("request");
 const puppet = require("puppeteer");
 
 (async () => {
@@ -10,7 +8,7 @@ const puppet = require("puppeteer");
     pipe: false
   });
 
-  console.log("getting new page");
+  console.log("getting new page...");
   const page = await browser.newPage();
   await page.setViewport({
     width: 1920,
@@ -30,37 +28,51 @@ const puppet = require("puppeteer");
   await page.waitFor(".video-react-fullscreen-control");
   await page.click(".video-react-fullscreen-control");
 
-  for (let i = 10; i >= 1; i--) {
+      process.stdout.write("taking screens in ");
+
+  for (let i = 3; i >= 1; i--) {
 		await page.waitFor(1000)
-    process.stdout.write("taking screens in ");
-    process.stdout.write(`${i}...`);
+    process.stdout.write(`${i}... `);
   }
   await page.evaluate(() => {
     document.querySelector(".video-react-control-bar").style.opacity = 0;
   });
 
   await page.click(".video-react-play-control");
+  await page.click('.video-react-fullscreen-control')
+''
+  let time = 0
+  let counter = 0
+  let duration = await page.$eval(
+    ".video-react-duration-display",
+    e => e.innerText
+  );
+  duration = duration.split("\n");
+  duration = duration[1].split(":");
+  duration = parseInt(duration[0]) * 60 + parseInt(duration[1]);
+  console.log(duration)
 
-  let counter = 0;
-  let time;
-  while (true) {
-    time = await page.$eval(
+  while (time <= duration) {
+     time = await page.$eval(
       ".video-react-current-time-display",
       e => e.innerText
     );
     time = time.split("\n");
     time = time[1].split(":");
     time = parseInt(time[0]) * 60 + parseInt(time[1]);
+
     if (time >= counter) {
       await page.click(".video-react-play-control"); // pause
       await page.waitForSelector(".video-react-loading-spinner", {
         hidden: true,
         timeout: 0
       });
-      process.stdout.write("snap!", time);
+
+      process.stdout.write(`snap ${time}/${duration} `);
       await page.screenshot({
-        path: `screencaps/screen${time}.png`
+        path: `screencaps/${time}.png`
       });
+
       await page.waitForSelector(".video-react-loading-spinner", {
         hidden: true
       });
